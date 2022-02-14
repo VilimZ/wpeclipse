0fc9144abfd2f6617be19439687787db33b088e4
Vilim <vilim.zovko@gmail.com>
2022-02-14 14:15:45 +0100

    Added contact page template file and content template. Style content templates.

---
+++ page-contact.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package wpeclipse
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+
+
+			get_template_part( 'template-parts/content', 'contact' );
+
+
+		?>
+
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
